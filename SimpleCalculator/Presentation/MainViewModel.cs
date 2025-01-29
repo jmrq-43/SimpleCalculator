29e12e4ab176297fd96035e8dbeea6d673778085
jmrq-43 <rojasquinteroj26@gmail.com>
2025-01-28 22:24:31 -0500

    added UI configuration

--- SimpleCalculator/Presentation/MainViewModel.cs
+++ SimpleCalculator/Presentation/MainViewModel.cs
@@ -2,29 +2,32 @@ namespace SimpleCalculator.Presentation;
 
 public partial class MainViewModel : ObservableObject
 {
-    private INavigator _navigator;
+    private readonly IThemeService _themeService;
+
+    public MainViewModel(IThemeService themeService)
+    {
+        _themeService = themeService;
+        IsDark = _themeService.IsDark;
+        _themeService.ThemeChanged += (_, _) => IsDark = _themeService.IsDark;
+    }
+
+    private bool _isDark;
+    public bool IsDark
+    {
+        get => _isDark;
+        set
+        {
+            if (SetProperty(ref _isDark, value))
+            {
+                _themeService.SetThemeAsync(value ? AppTheme.Dark : AppTheme.Light);
+            }
+        }
+    }
 
     [ObservableProperty]
-    private string? name;
+    private Calculator _calculator = new();
 
-    public MainViewModel(
-        IStringLocalizer localizer,
-        IOptions<AppConfig> appInfo,
-        INavigator navigator)
-    {
-        _navigator = navigator;
-        Title = "Main";
-        Title += $" - {localizer["ApplicationName"]}";
-        Title += $" - {appInfo?.Value?.Environment}";
-        GoToSecond = new AsyncRelayCommand(GoToSecondView);
-    }
-    public string? Title { get; }
-
-    public ICommand GoToSecond { get; }
-
-    private async Task GoToSecondView()
-    {
-        await _navigator.NavigateViewModelAsync<SecondViewModel>(this, data: new Entity(Name!));
-    }
-
+    [RelayCommand]
+    private void Input(string key) =>
+        Calculator = Calculator.Input(key);
 }
